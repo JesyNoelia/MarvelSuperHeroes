756880559369b9fd5f6048023760ca28ecde55af
jesica.cepero <jesica.cepero@grupoamper.com>
2022-06-24 14:09:34 +0200

    working on UI, layouts

--- app/src/main/java/com/jesy/marvelsuperheroes/MainActivity.kt
+++ app/src/main/java/com/jesy/marvelsuperheroes/MainActivity.kt
@@ -2,12 +2,21 @@ package com.jesy.marvelsuperheroes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import com.jesy.marvelsuperheroes.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
     }
 }
---
+++ app/src/main/java/com/jesy/marvelsuperheroes/ui/CharactersList/CharacterListFragment.kt
@@ -0,0 +1,7 @@
+package com.jesy.marvelsuperheroes.ui.CharactersList
+
+import androidx.fragment.app.Fragment
+import com.jesy.marvelsuperheroes.R
+
+class CharacterListFragment : Fragment(R.layout.fragment_list){
+}
